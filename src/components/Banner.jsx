a4393e220233bc274027d2afe483c67a293b16da
maheshsaveasweb <mahesh.saveasweb101@gmail.com>
2024-11-18 21:48:58 +0530

    added d

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,50 @@
+import { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import ExampleCarouselImage from "./ExampleCarouselImage";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import { Image } from "react-bootstrap";
 
 function Banner() {
+  const [banner, setBanner] = useState([]);
+  useEffect(() => {
+    async function getBanner() {
+      try {
+        const response = await fetch("http://localhost:4000/get-banner");
+        if (response) {
+          const data = await response.json();
+          // console.log("banner data", data.data);
+          setBanner(data.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getBanner();
+  }, []);
   return (
     <Carousel className="mt-5 mb-5">
-      <Carousel.Item>
+      {banner.map((item) => {
+        return (
+          <Carousel.Item>
+            <Container>
+              <Row>
+                <Col className="d-flex justify-content-center align-items-center">
+                  <div>
+                    <h1>{item.heading}</h1>
+                  </div>
+                </Col>
+                <Col>
+                  <Image src={item.image} alt="banner image" />
+                </Col>
+              </Row>
+            </Container>
+          </Carousel.Item>
+        );
+      })}
+      {/* <Carousel.Item>
         <Container>
           <Row>
             <Col className="d-flex justify-content-center align-items-center">
@@ -19,12 +56,8 @@ function Banner() {
               <ExampleCarouselImage text="First slide" />
             </Col>
           </Row>
-        </Container>
-
-        {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
+        </Container>{" "}
+   
       </Carousel.Item>
       <Carousel.Item>
         <Container>
@@ -39,31 +72,8 @@ function Banner() {
             </Col>
           </Row>
         </Container>{" "}
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-        <Container>
-          <Row>
-            <Col className="d-flex justify-content-center align-items-center">
-              <div>
-                <h1>Your search for a CFO ends here</h1>
-              </div>
-            </Col>
-            <Col>
-              <ExampleCarouselImage text="First slide" />
-            </Col>
-          </Row>
-        </Container>{" "}
-        {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
+   
+      </Carousel.Item> */}
     </Carousel>
   );
 }
