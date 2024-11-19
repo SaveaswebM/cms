b727c2f51fd6b5a5555d8677725037c8b9de9f73
maheshsaveasweb <mahesh.saveasweb101@gmail.com>
2024-11-19 19:52:47 +0530

    added why section api

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/container";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/Row";
@@ -6,6 +6,20 @@ import Image from "react-bootstrap/esm/Image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 function Why() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getApiData() {
+      const response = await fetch("http://localhost:4000/get-cfo-support");
+      if (response) {
+        const data = await response.json();
+        // console.log("api data", data);
+        setData(data.data[0]);
+      }
+    }
+    getApiData();
+  }, []);
+  console.log("list", data.list);
   return (
     <>
       <div className="why-bg-color container-top-space">
@@ -16,7 +30,10 @@ function Why() {
               md={12}
               className="position-relative d-flex justify-content-center z-0"
             >
-              <div className="mask-img"></div>
+              <div
+                className="mask-img"
+                style={{ backgroundImage: ` url(${data.image})` }}
+              ></div>
               <div className="shape">
                 <Image
                   src="https://cfosupport.in/assets/img/images/about_shape01.png"
@@ -26,12 +43,40 @@ function Why() {
             </Col>
             <Col lg={6} md={12}>
               <div>
-                <h1 className="text-uppercase">cfo support</h1>
-                <h4 className="text-space">
-                  Driving success virtually and seamlessly
-                </h4>
+                <h1 className="text-uppercase">{data.title}</h1>
+                <h4 className="text-space">{data.subTitle}</h4>
                 <ul className="pl-0">
-                  <li className="custom-list ">
+                  {data.list.map((item) => {
+                    return (
+                      <li className="custom-list ">
+                        <Row>
+                          <Col xl={2}>
+                            {" "}
+                            <div>
+                              <FontAwesomeIcon
+                                className="icon-wrapper"
+                                icon={faEnvelope}
+                                size="2xl"
+                                style={{ color: "#74C0FC" }}
+                              />
+                            </div>
+                          </Col>
+                          <Col xl={9}>
+                            {" "}
+                            <div>
+                              <h6>PROFESSIONAL ADVICE</h6>
+                              <p>
+                                Expert CFO guidance, without the high cost.
+                                Partnering for business success
+                              </p>
+                            </div>
+                          </Col>
+                        </Row>
+                      </li>
+                    );
+                  })}
+
+                  {/* <li className="custom-list ">
                     <Row>
                       <Col xl={2}>
                         {" "}
@@ -105,32 +150,7 @@ function Why() {
                         </div>
                       </Col>
                     </Row>
-                  </li>
-                  <li className="custom-list ">
-                    <Row>
-                      <Col xl={2}>
-                        {" "}
-                        <div>
-                          <FontAwesomeIcon
-                            className="icon-wrapper"
-                            icon={faEnvelope}
-                            size="2xl"
-                            style={{ color: "#74C0FC" }}
-                          />
-                        </div>
-                      </Col>
-                      <Col xl={9}>
-                        {" "}
-                        <div>
-                          <h6>PROFESSIONAL ADVICE</h6>
-                          <p>
-                            Expert CFO guidance, without the high cost.
-                            Partnering for business success
-                          </p>
-                        </div>
-                      </Col>
-                    </Row>
-                  </li>
+                  </li> */}
                 </ul>
               </div>
             </Col>
